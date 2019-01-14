0550d85c56ceaf051c65d284f8b768e519a7645a
superMrl <q55667890@163.com>
2019-01-14 22:32:11 +0800

    修改pom文件中springboot和eureka的版本

--- src/main/java/com/sbc/manager/RegisterApplication.java
+++ src/main/java/com/sbc/manager/RegisterApplication.java
@@ -1,12 +1,12 @@
 package com.sbc.manager;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+
 @SpringBootApplication
+@EnableEurekaServer
 public class RegisterApplication{
 
     public static void main(String[] args) {
